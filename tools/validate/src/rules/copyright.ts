273b5042fb392aeb64d8b8c90f9ab18cda5307df
Olaf Tomalka <olaf@tomalka.me>
2022-09-29 16:03:26 -0500

    Added remark-lint critical rules and support for footnotes in copyright rule

--- tools/validate/src/rules/copyright.ts
+++ tools/validate/src/rules/copyright.ts
@@ -28,9 +28,22 @@ const COPYRIGHT_POSTAMBLE = [
 ];
 
 const rule = lintRule<Root>("sip:copyright", (tree, file) => {
+  // There might be definitions and footnotes after a copyright, they are allowed as they're markup and not content
+  let definitionsCount = 0;
+  while (
+    ["definition", "footnoteDefinition"].includes(
+      tree.children.at(-1 - definitionsCount)?.type ?? "<error>"
+    )
+  ) {
+    definitionsCount++;
+  }
+
   if (
-    tree.children.length < 2 ||
-    !deepContains(tree.children.slice(-2), COPYRIGHT_POSTAMBLE)
+    tree.children.length - definitionsCount < 2 ||
+    !deepContains(
+      tree.children.slice(-2 - definitionsCount),
+      COPYRIGHT_POSTAMBLE
+    )
   ) {
     const place = location(file);
     file.message(
